3586046394c1f9a5c14a3f59e3915abc5c9e5f41
nazabrahams <nazabrahams07@gmail.com>
2024-04-13 12:41:53 +0200

    staging file

---
+++ nazneen.js
@@ -0,0 +1,39 @@
+ /* This is a comment single-line comment
+ hjkhuhug */
+
+ //declare a variable That is an integer
+ let age = 20 
+ const x = 40
+ var deci = 20.5
+
+ //string
+ let firstname = "nazneen" //double quotes
+ let name = 'nazneen' //single quotes
+ let sentence = `my name is nazneen and i am ${age} years old` //template literals
+ let newSentence = 'my name is nazneen and i am ' + age + ' years old' //template literals
+ //concatenate//concatanation
+
+ //() parenthesis
+ //[] brackets
+ //{} braces 
+
+ //boolean
+ let answer = true
+ let answer2 = false
+ console.log(answer);
+
+ //null
+ let anynameforvariable = null 
+ let variable = null 
+
+ //object
+ let obj = {
+    name: 'nazneen' ,
+    surname: 'abrahams',
+    colour: 'purple'
+ }
+
+ //array -- indexiing/ uisng an or the index
+ let array = ['nazneen' ,true,20.4, 'abrahams']
+
+ 
